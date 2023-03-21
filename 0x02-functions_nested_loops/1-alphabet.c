4c4369af84ee5ae4b2b7f914dbdfa0f6db7ea7c0
hamada <chiba.mohamed@gmail.cols>
2023-03-20 22:10:17 -0700

    add 01-03

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * return: 0
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
+
