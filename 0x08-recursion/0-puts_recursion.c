803e49e89dfb8827395cd21ea4ff2e96dcf92930
hamada <chiba.mohamed@gmail.cols>
2023-04-05 12:48:22 -0700

    08add

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,6 @@
-/*                                                                                                                                      
- * File: 0-puts_recursion.c                                                                                                             
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
 #include "main.h"                                                                                                                       
                                                                                                                                         
-/**                                                                                                                                     
+/*                                                                                                                                     
  * _puts_recursion - Prints a string, followed by a new line.                                                                           
  * @s: The string to be printed.                                                                                                        
  */                                                                                                                                     
